f45797f1539295979c0796744bd96840c0e63a0c
Harsh <wharsh28@gmail.com>
2025-01-10 23:00:14 +0530

    Frontend Functionality

--- Practice Project/index.js
+++ Practice Project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -8,8 +9,18 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-  res.render('index');
+  fs.readdir(`./files`,function(err,files){
+    res.render('index',{files:files});
+  })
 });
+
+app.post('/create', (req, res) => {
+fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+  if(err) throw err;
+  res.redirect('/');
+});
+  })
+
 
 app.listen(3000, function() {    
     console.log('Server is running on port 3000');
